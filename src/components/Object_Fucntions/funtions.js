708d06f0404b4a88a34f1ce4dd1f1d9a2e64d1bf
IHCantabraba <inigocanta@gmail.com>
2024-03-23 09:12:27 +0100

    position_tracker

--- src/components/Object_Fucntions/funtions.js
+++ src/components/Object_Fucntions/funtions.js
@@ -18,18 +18,42 @@ export function CreateMap() {
   // const mapDiv = document.querySelector('#map')
   // mapDiv.id = 'map'
 
-  const map = L.map('map').setView([43.194104, -4.832371], 13)
+  const map = L.map('map').setView([40.558047, -4.620497], 6)
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map)
-  var circle = L.circle([43.194104, -4.832371], {
-    color: 'blue',
-    fillColor: 'lightblue',
-    fillOpacity: 0.5,
-    radius: 1000
-  }).addTo(map)
+
+  if (!navigator.geolocation) {
+    conlog.log(`Your browser doesn't support golocation feature`)
+  } else {
+    /* run the geolocationn function every 5 seconds*/
+    setInterval(() => {
+      navigator.geolocation.getCurrentPosition(getPosition)
+    }, 2000)
+  }
+  var marker, circle
+  function getPosition(position) {
+    console.log(position)
+    var lat = position.coords.latitude
+    var long = position.coords.longitude
+    var accuracy = position.coords.accuracy
+    if (marker) {
+      map.removeLayer(marker)
+    }
+    if (circle) {
+      map.removeLayer(circle)
+    }
+    var marker = L.marker([lat, long]).addTo(map)
+    var circle = L.circle([lat, long], { raduis: accuracy }).addTo(map)
+    var featureGroup = L.featureGroup([marker, circle]).addTo(map)
+    map.fitBounds(featureGroup.getBounds())
+    // var popup = L.popup().setLatLng([lat, long]).setContent(' Loc.').openOn(map)
+    console.log(
+      `Latitudeis : ${lat}, Longitude is: ${long} and accuracy is: ${accuracy}`
+    )
+  }
 
   return map
 }
