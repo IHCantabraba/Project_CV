59c1b8860d14e8387c766ad2e6e7f9cb4ff08b1f
IHCantabraba <inigocanta@gmail.com>
2024-03-23 11:25:50 +0100

    tracking line

--- src/components/Object_Fucntions/funtions.js
+++ src/components/Object_Fucntions/funtions.js
@@ -1,6 +1,6 @@
 /* funtion to create a Ul with li > a elements for element in Array */
 import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
+import L, { polyline } from 'leaflet'
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 export function CreateUl(DataArray) {
@@ -31,20 +31,94 @@ export function CreateMap() {
       navigator.geolocation.getCurrentPosition(getPosition)
     }, 2000)
   }
-  let marker, circle, greenIcon, defaultmarker
+  let marker, circle, greenIcon, polyline
+  let HistoricalLocatios
   function getPosition(position) {
-    console.log(position)
     var lat = position.coords.latitude
     var long = position.coords.longitude
+    if (!HistoricalLocatios) {
+      HistoricalLocatios = []
+      let FirstPosition = [lat, long]
+      HistoricalLocatios.push(FirstPosition)
+    }
+
     var accuracy = position.coords.accuracy
     /* eliminar posiciones anteriores */
     /* hacer que se pueda elegir con un botton */
     const btnTracking = document.querySelector('.Tracking-btn')
     if (btnTracking.classList.contains('hide')) {
-      console.log('removing prior objects')
       /* TODO ¿ how to read al markers? */
       map.eachLayer((layer) => {
-        if (layer) {
+        btnTracking.innerHTML = 'Show Tracking!'
+        /* eliminar cilculo accuracy */
+        if (layer.options.title === 'circle') {
+          map.removeLayer(layer)
+        }
+        /* eliminar el marcador previo obtener su posicion y almacenarla */
+        if (layer.options.title === 'marker') {
+          /* obtener coordenadas previas del marcador */
+          const previousLatLong = layer.getLatLng()
+          /* obtener posicion */
+          lat = previousLatLong['lat']
+          long = previousLatLong['lng']
+          /* almacenar posicion */
+          const latlot = [lat, long]
+          HistoricalLocatios.push(latlot)
+          map.removeLayer(layer)
+          /* añadir a la lista de posiciones */
+        } /* si es una línea eliminarla */
+        if (layer.options.title === 'line') {
+          map.removeLayer(layer)
+        }
+      })
+      greenIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow,
+        iconAnchor: [14, 40],
+        popupAnchor: [0, 0],
+        tooltipAnchor: [0, 0]
+      })
+      // defaultmarker = L.marker([lat, long]).addTo(map)
+      marker = L.marker([lat, long], {
+        title: 'marker',
+        alt: 'marker',
+        icon: greenIcon
+      }).addTo(map) //{ icon: greenIcon }
+      circle = L.circle([lat, long], {
+        title: 'circle',
+        alt: 'circle',
+        raduis: accuracy
+      }).addTo(map)
+
+      polyline = L.polyline(HistoricalLocatios, {
+        title: 'line',
+        alt: 'line',
+        raduis: accuracy
+      }).addTo(map)
+
+      var featureGroup = L.featureGroup([marker, circle, polyline]).addTo(map)
+      map.fitBounds(featureGroup.getBounds())
+    } /*Si queremos mostrar el track */ else {
+      btnTracking.innerHTML = 'Tracking You!'
+      console.log('Tracking active')
+      map.eachLayer((layer) => {
+        /* eliminar cilculo accuracy */
+        if (layer.options.title === 'circle') {
+          map.removeLayer(layer)
+        }
+        if (layer.options.title === 'marker') {
+          /* obtener coordenadas previas del marcador */
+          const previousLatLong = layer.getLatLng()
+          /* obtener posicion */
+          lat = previousLatLong['lat']
+          long = previousLatLong['lng']
+          /* almacenar posicion */
+          const latlot = [lat, long]
+          HistoricalLocatios.push(latlot)
+          map.removeLayer(layer)
+          /* añadir a la lista de posiciones */
+        } /* si es una línea eliminarla */
+        if (layer.options.title === 'line') {
           map.removeLayer(layer)
         }
       })
@@ -58,13 +132,24 @@ export function CreateMap() {
       tooltipAnchor: [0, 0]
     })
     // defaultmarker = L.marker([lat, long]).addTo(map)
-    marker = L.marker([lat, long], { title: 'marker', icon: greenIcon }).addTo(
-      map
-    ) //{ icon: greenIcon }
-    circle = L.circle([lat, long], { title: 'circle', raduis: accuracy }).addTo(
-      map
-    )
-    var featureGroup = L.featureGroup([marker, circle]).addTo(map)
+    marker = L.marker([lat, long], {
+      title: 'marker',
+      alt: 'marker',
+      icon: greenIcon
+    }).addTo(map) //{ icon: greenIcon }
+    // circle = L.circle([lat, long], {
+    //   title: 'circle',
+    //   alt: 'circle',
+    //   raduis: accuracy
+    // }).addTo(map)
+
+    polyline = L.polyline(HistoricalLocatios, {
+      title: 'line',
+      alt: 'line',
+      raduis: accuracy
+    }).addTo(map)
+
+    var featureGroup = L.featureGroup([marker, polyline]).addTo(map)
     map.fitBounds(featureGroup.getBounds())
     // var popup = L.popup().setLatLng([lat, long]).setContent(' Loc.').openOn(map)
   }
